e3ba417e68df70fa5ab82868187ea5a9c6f8ea34
riveraeu <riveraeu@sonoma.edu>
2018-06-08 16:59:54 -0700

    image optimization

--- .nuxt/axios.js
+++ .nuxt/axios.js
@@ -113,8 +113,8 @@ export default (ctx, inject) => {
   const axiosOptions = {
     // baseURL
     baseURL : process.browser
-      ? 'http://localhost:3000/'
-      : (process.env._AXIOS_BASE_URL_ || 'http://localhost:3000/'),
+      ? 'http://www.hilightproductions.com/'
+      : (process.env._AXIOS_BASE_URL_ || 'http://www.hilightproductions.com/'),
 
     // Create fresh objects for all default header scopes
     // Axios creates only one which is shared across SSR requests!
